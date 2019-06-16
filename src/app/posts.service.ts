cb5d7b6817a6f3e2c90b437d667057fd827726eb
Your Name <you@example.com>
2019-06-16 20:59:08 +0200

    initial commit

---
+++ src/app/posts.service.ts
@@ -0,0 +1,79 @@
+import { Injectable } from '@angular/core'
+import { HttpClient, HttpHeaders, HttpParams, HttpEventType } from '@angular/common/http';
+
+import { Post } from './post.model';
+import { map, catchError, tap } from 'rxjs/operators';
+import { Subject, throwError } from 'rxjs';
+
+
+
+@Injectable({providedIn: 'root'})
+    
+  export class PostsService {
+    error = new Subject <string>();
+
+    constructor(private http: HttpClient){}
+    
+    createAndStorePost(title: string, content:string){
+        const postData: Post ={title: title, content: content}
+
+        this.http
+        .post<{ name:string }>(
+          'https://angular-kursv2.firebaseio.com/posts.json',
+          postData,
+          {
+            observe: 'response',
+            responseType: 'json'
+          }
+        )
+        .subscribe(responseData => {
+         console.log(responseData);
+        }, error => {
+          this.error.next(error.message);
+        });
+    }
+
+    fetchPosts(){
+    //  let searchParams = new HttpParams();
+     // searchParams = searchParams.append('print', 'pretty');
+
+      return this.http.get<{ [key: string]: Post}>('https://angular-kursv2.firebaseio.com/posts.json',
+      {
+        headers: new HttpHeaders({'Custom-Header': 'Hello'}),
+        params: new HttpParams().set('print', 'pretty')
+      }
+      )
+      .pipe(
+        map(responseData =>{
+        const postArray: Post[] = [];
+        for (const key in responseData){
+          if (responseData.hasOwnProperty(key)){
+            postArray.push({ ...responseData[key], id: key });
+          }
+        }
+          return postArray
+      }),
+        catchError(errorRes =>{
+         return throwError(errorRes);
+        })
+      
+      );   
+  }
+    
+    clearPosts(){
+      return this.http.delete('https://angular-kursv2.firebaseio.com/posts.json',
+      {
+        observe: 'events',
+        responseType: 'json'
+      }
+      ).pipe(tap(event => {
+        console.log(event);
+        if (event.type=== HttpEventType.Response){
+          console.log(event.body);
+        }
+      }));
+    }
+
+
+    }
+
